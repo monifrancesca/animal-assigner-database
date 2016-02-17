e8f1f7b4c05eda722147a5fa0b49381c60ff8961
Saviohieu Nguyen <Hieu@savionguyen.local>
2016-02-17 16:55:33 -0600

    first commit

---
+++ routes/name.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var nameArray = [];
+
+
+router.get('/', function(req, res) {
+    res.send(nameArray);
+});
+
+router.post('/', function(req, res) {
+    nameArray.push(req.body);
+    console.log(req.body);
+    res.send(nameArray);
+    console.log(nameArray);
+});
+
+module.exports = router;
+module.exports.nameArray = nameArray;
